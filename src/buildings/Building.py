27b26774ed8f755697c065390da6d74158cb11b1
Audran <audran.bert@alumni.univ-avignon.fr>
2020-12-09 10:45:04 +0100

    buildings

---
+++ src/buildings/Building.py
@@ -0,0 +1,15 @@
+
+
+class Building:
+    """
+    classe batiment
+    """
+    def __init__(self):
+        """Constructeur de notre classe"""
+        print("building")
+    def __str__(self):
+        """Méthode appelée lors d'une conversion de l'objet en chaîne"""
+        return "building affichage"
+
+    def apply():
+        print("effect")
---
+++ src/buildings/University.py
@@ -0,0 +1,11 @@
+from Building import *
+
+
+class University(Building):
+    """
+    classe University
+    """
+    def __init__(self):
+        """Constructeur de notre classe"""
+        super()
+
