80cd4c9615430ece671c21e0ab12bdbbef932ee6
Damien Dallon <damien.dallon@alumni.univ-avignon.fr>
2020-12-09 11:45:31 +0100

    modify Character class and create Standard, Hippy and HurriedMan classes

--- src/character.py
+++ src/character.py
@@ -1,13 +1,49 @@
 class Character:
 
-    def __init__(self, life, hydration, satiety, mentality, sprite, position, go_position):
+    def __init__(self, life, hydration, satiety, mentality, sprite, game, position, go_position, movement_mode, has_swimsuit):
         self.life = life
-        self.hyration = hydration
+        self.hydration = hydration
         self.satiety = satiety
         self.mentality = mentality
         self.sprite = sprite
+        self.game = game                                # Liaison entre la ville et le personnage
         self.position = position                        # Position x et y courante
         self.go_position = go_position                  # Position x et y suivante
+        self.movement_mode = movement_mode
+        self.has_swimsuit = has_swimsuit
 
-        def move():
-            pass
+    def move(self):
+        pass
+
+    def display(self, screen):
+        pass
+
+class Standard(Character):
+
+    def __init__(self, sprite, game, position, go_position, movement_mode, has_swimsuit):
+        super().__init__(75, 75, 75, 75, sprite, game, position, go_position, movement_mode, has_swimsuit)
+    
+    def move(self):
+        self.life -= 1
+        self.hydration -= 1
+        self.satiety -= 1
+        self.mentality -= 1
+
+
+class Hippy(Character):
+
+    def __init__(self, sprite, game, position, go_position, movement_mode, has_swimsuit):
+        super().__init__(75, 50, 50, 100, sprite, game, position, go_position, movement_mode, has_swimsuit)
+    
+    def move(self):
+        self.life -= 2
+        self.hydration -= 2
+        self.satiety -= 2
+
+class HurriedMan(Character):
+
+    def __init__(self, sprite, game, position, go_position, movement_mode, has_swimsuit):
+        super().__init__(100, 75, 75, 50, sprite, game, position, go_position, movement_mode, has_swimsuit)
+    
+    def move(self):
+        self.mentality -= 2
