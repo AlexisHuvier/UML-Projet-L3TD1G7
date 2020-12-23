e3d772d5e11940bbc8618b43e22a5dc64d2e2973
Audran <audran.bert@alumni.univ-avignon.fr>
2020-12-23 13:16:39 +0100

    Amelioration de la generation de la map

--- src/tiles/Map.py
+++ src/tiles/Map.py
@@ -18,17 +18,14 @@ class Map:
     """
     case=[]
 
-    def generateTile(self,p):
-        if (p==1):
+    def generateTile(self,p,x,y):
+        p=random.randint(0,100)
+        if (p<33):
             T=Forest()
-        elif (p==2):
+        elif (p<66):
             T=GreyTile()
-        elif (p==3):
-            T=Road()
-        elif (p==4):
+        elif (p<101):
             T=WaterTile()
-        elif (p==5):
-            T=Sidewalk()
         return T
 
 
@@ -39,49 +36,113 @@ class Map:
         for i in range(x):
             self.case.append([])
             for j in range(y):
-                p=random.randint(1,6)
-                if (p==6):
-                    p=random.randint(0,4)
-                    if (b2[p]==0):
-                        b2[p]=1
-                        if (p==0): 
-                            T=FastFood()
-                        elif (p==1):
-                            T=House()
-                            housePos=[i,j]
-                        elif (p==2):
-                            T=Library()
-                        elif (p==3):
-                            T=Pub()
-                        elif (p==4):
-                            T=University()
-                    else :
-                        p=random.randint(1,5)
-                        T=self.generateTile(p)
-                else :
-                    T=self.generateTile(p)  
+                p=random.randint(1,5)
+##                if (p==6):
+##                    p=random.randint(0,4)
+##                    if (b2[p]==0):
+##                        b2[p]=1
+##                        if (p==0): 
+##                            T=FastFood()
+##                        elif (p==1):
+##                            T=House()
+##                            housePos=[i,j]
+##                        elif (p==2):
+##                            T=Library()
+##                        elif (p==3):
+##                            T=Pub()
+##                        elif (p==4):
+##                            T=University()
+##                    else :
+##                        p=random.randint(1,5)
+##                        T=self.generateTile(p,i,j)
+##                else :
+                T=self.generateTile(p,i,j)  
                 self.case[i].append(T)
+        for i in range(x*y//12):
+            x1=random.randint(0,x-1)
+            y1=random.randint(0,y-1)
+            self.case[x1][y1]=Road()
+        for i in range(x*y//12):
+            x1=random.randint(0,x-1)
+            y1=random.randint(0,y-1)
+            self.case[x1][y1]=Sidewalk()
+        for i in range(x):
+            for j in range(y):
+                if (issubclass(self.case[i][j].__class__,Road)):
+                    x2=i
+                    y2=j
+                    r=random.randint(0,1)
+                    if (r==0):
+                        r=random.randint(0,1)
+                        if (r==0):
+                            while (x2>=0):
+                                x2=x2-1
+                                self.case[x2][y2]="R"
+                        else :
+                            while (x2<x-1):
+                                x2=x2+1
+                                self.case[x2][y2]="R"                     
+                    else :
+                        r=random.randint(0,1)
+                        if (r==0):
+                            while (y2>=0):
+                                y2=y2-1
+                                self.case[x2][y2]="R" 
+                        else :
+                            while (y2<y-1):
+                                y2=y2+1
+                                self.case[x2][y2]="R"
+                elif (issubclass(self.case[i][j].__class__,Sidewalk)):
+                    x2=i
+                    y2=j
+                    r=random.randint(0,1)
+                    if (r==0):
+                        r=random.randint(0,1)
+                        if (r==0):
+                            while (x2>=0):
+                                x2=x2-1
+                                self.case[x2][y2]="S"
+                        else :
+                            while (x2<x-1):
+                                x2=x2+1
+                                self.case[x2][y2]="S"                     
+                    else :
+                        r=random.randint(0,1)
+                        if (r==0):
+                            while (y2>=0):
+                                y2=y2-1
+                                self.case[x2][y2]="S" 
+                        else :
+                            while (y2<y-1):
+                                y2=y2+1
+                                self.case[x2][y2]="S"
+        for i in range(x):
+            for j in range(y):
+                if(self.case[i][j]=="R"):
+                    self.case[i][j]=Road()
+                if(self.case[i][j]=="S"):
+                    self.case[i][j]=Sidewalk()
         for i in range(len(b2)):
             if (b2[i]==0):
                 b2[i]=1
-                if (p==0): 
+                if (i==0): 
                     T=FastFood()
-                elif (p==1):
+                elif (i==1):
                     T=House()
                     housePos=[i,j]
-                elif (p==2):
+                elif (i==2):
                     T=Library()
-                elif (p==3):
+                elif (i==3):
                     T=Pub()
-                elif (p==4):
+                elif (i==4):
                     T=University()
-                x1=random.randint(0,x-1)
+                x2=random.randint(0,x-1)
                 y2=random.randint(0,y-1)
-                if (not(issubclass(self.case[x1][y2].__class__,Building))):
-                    print(not(issubclass(self.case[x1][y2].__class__,Building)))
-                    self.case[x1][y2]=T
-                else :
-                    print("not spawn")
+                while ((issubclass(self.case[x2][y2].__class__,Building))):
+                    print(not(issubclass(self.case[x2][y2].__class__,Building)),self.case[x2][y2].__class__)
+                    x2=random.randint(0,x-1)
+                    y2=random.randint(0,y-1)
+                self.case[x2][y2]=T
         return housePos
     
     def __init__(self):
